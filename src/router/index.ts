961ec696fb6931a76e7862c578e7bf1902a35657
vakesamahere <vakegamer@163.com>
2025-08-17 21:33:37 +0800

    feat: 设置页面标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,22 @@
+import App from '@/App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: App,
+      meta: { title: 'Cue Aimer' }
+    },
+  ],
 })
+
+router.beforeEach((to, from, next) => {
+  // 从路由的元信息中获取标题
+  document.title = to.meta.title as string || 'Cue Aimer';
+  next();
+});
 
 export default router
